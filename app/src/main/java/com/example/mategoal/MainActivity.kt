42145695fe6fc05d3007ecb50353bbbceb90e46b
huwil <william-za.hu@ubs.com>
2019-08-11 15:00:54 +0800

    0811 Add google ad instruction.

--- app/src/main/java/com/example/mategoal/MainActivity.kt
+++ app/src/main/java/com/example/mategoal/MainActivity.kt
@@ -10,3 +10,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//Google Ad install
+//Next, place the ad unit inside your app
+//Follow these instructions:
+//Complete the instructions in the Google Mobile Ads SDK guide using this app ID:
+//MateGoalca-app-pub-6835894022327289~6430573643
+//Follow the banner implementation guide to integrate the SDK. You'll specify ad type, size, and placement when you integrate the code using this ad unit ID:
+//holidaysca-app-pub-6835894022327289/9744693196
+//Review the AdMob policies to ensure your implementation complies.
+//EMAIL INSTRUCTIONS
