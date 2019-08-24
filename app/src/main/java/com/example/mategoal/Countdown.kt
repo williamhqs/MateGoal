2e6d5c5ee712b16a7ae96daed6fa5ae8c5470e0d
huwil <william-za.hu@ubs.com>
2019-08-24 11:09:31 +0800

    .ff.

---
+++ app/src/main/java/com/example/mategoal/Countdown.kt
@@ -0,0 +1,3 @@
+package com.example.mategoal
+
+data class Countdown (val name: String = "", val count: Int = 0)
--- app/src/main/java/com/example/mategoal/HolidayAdapter.kt
+++ app/src/main/java/com/example/mategoal/HolidayAdapter.kt
@@ -3,21 +3,29 @@ package com.example.mategoal
 import android.os.Parcel
 import android.os.Parcelable
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
 class HolidayAdapter(private val holidays: List<String>) : RecyclerView.Adapter<HolidayAdapter.HolidayViewHolder>() {
 
-    class HolidayViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {}
+    class HolidayViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val leftDays = view.findViewById<TextView>(R.id.left_days)
+        val title = view.findViewById<TextView>(R.id.title)
+    }
 
     override fun getItemCount(): Int = holidays.size
 
     override fun onBindViewHolder(p0: HolidayViewHolder, p1: Int) {
-        p0.textView.text = holidays[p1]
+        p0.title.text = holidays[p1]
+
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): HolidayViewHolder {
-        return HolidayViewHolder(TextView(p0.context))
+        val view = LayoutInflater.from(p0.context).inflate(R.layout.holiday_item, p0, false)
+
+        return HolidayViewHolder(view)
     }
 
 }
