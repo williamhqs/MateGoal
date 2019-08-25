4e31b94ea76675dbc644dfa35b65c43f1d073236
huwil <william-za.hu@ubs.com>
2019-08-26 00:29:41 +0800

    0826 Show holiday list.

--- app/src/main/java/com/example/mategoal/HolidayAdapter.kt
+++ app/src/main/java/com/example/mategoal/HolidayAdapter.kt
@@ -7,19 +7,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import org.json.JSONArray
+import org.json.JSONObject
 
-class HolidayAdapter(private val holidays: List<String>) : RecyclerView.Adapter<HolidayAdapter.HolidayViewHolder>() {
+class HolidayAdapter(private val holidays: JSONArray) : RecyclerView.Adapter<HolidayAdapter.HolidayViewHolder>() {
 
     class HolidayViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val leftDays = view.findViewById<TextView>(R.id.left_days)
+//        val leftDays = view.findViewById<TextView>(R.id.left_days)
         val title = view.findViewById<TextView>(R.id.title)
     }
 
-    override fun getItemCount(): Int = holidays.size
+    override fun getItemCount(): Int = holidays.length()
 
     override fun onBindViewHolder(p0: HolidayViewHolder, p1: Int) {
-        p0.title.text = holidays[p1]
 
+        val holiday = holidays[p1] as JSONObject
+        //p0.title.text = holiday["summary"] as String
+        p0.title.text = (holiday["summary"] as String) + p1.toString()
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): HolidayViewHolder {
--- app/src/main/java/com/example/mategoal/MainActivity.kt
+++ app/src/main/java/com/example/mategoal/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mategoal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -11,37 +12,51 @@ import com.android.volley.Response.Listener
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
 import java.net.URL
-''
-class MainActivity : AppCompatActivity() {
-A`,==]=----
-    val items = listOf(
-        "给初学者的RxJava2.0教程（七）: Flowable",
-        "Android之View的诞生之谜",
-        "Android之自定义View的死亡三部曲之Measure",
-        "Using ThreadPoolExecutor in Android ",
-        "Kotlin 泛型定义与 Java 类似，但有着更多特性支持。",
-        "Android异步的姿势，你真的用对了吗？",
-        "Android 高质量录音库。",
-        "Android 边缘侧滑效果，支持多种场景下的侧滑退出。"
-    )
 
+class MainActivity : AppCompatActivity() {
+    var items = listOf(mapOf<String, Any?>())
+    var holidays = JSONArray()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+
         val listView = findViewById<RecyclerView>(R.id.listView)
         listView.layoutManager = LinearLayoutManager(this)
-        listView.adapter = HolidayAdapter(items)
+
 
 
         val requestQueue = Volley.newRequestQueue(this)
         val url = "http://www.mategoal.com:8090/holiday/zh_cn.china"
         val request = StringRequest(url, Listener { response ->
-
-            Log.d("123", "123")
-            Log.d("123", response)
+//
+//            Log.d("123", "123")
+//            Log.d("123", response)
 //            Json
+//            val json = Json(JsonConfiguration.Stable)
+            // serializing objects
+//            val jsonData = json.stringify(Data.serializer(), Data(42))
+//            // serializing lists
+//            val jsonList = json.stringify(Data.serializer().list, listOf(Data(42)))
+//            println(jsonData) // {"a": 42, "b": "42"}
+//            println(jsonList) // [{"a": 42, "b": "42"}]
+//
+//            // parsing data back
+//            val obj = json.parse(Data.serializer(), """{"a":42}""") // b is optional since it has default value
+//            println(obj) // Data(a=42, b="42"
+
+//            val holidays = JSONArray("[" + response + "]")
+            val json = JSONObject(response)
+            holidays = json["items"] as JSONArray
+            println(holidays.length())
+            val adapter =  HolidayAdapter(holidays)
+            listView.adapter = adapter
+//            adapter.notifyDataSetChanged()
 
         }, Response.ErrorListener { error ->
 
